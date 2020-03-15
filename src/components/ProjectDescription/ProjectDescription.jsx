1955b6dc5ad8024dac0cd30fe62b0e98337584ad
dyaroshevych <dyaroshevych@gmail.com>
2020-03-16 01:09:33 +0300

    mainly finished project descriptions

--- src/components/ProjectDescription/ProjectDescription.jsx
+++ src/components/ProjectDescription/ProjectDescription.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaTimes, FaExternalLinkAlt } from "react-icons/fa";
 import { Fade, Slide } from "react-reveal";
 
-import { Button, List } from "../";
+import { Button, Image, List } from "../";
 
 import "./ProjectDescription.scss";
 
@@ -10,101 +10,122 @@ const ProjectDescription = ({
   name,
   description,
   technologies,
+  process,
   repo,
   link,
   images,
   closeProject
-}) => (
-  <Slide bottom duration={500} enter={true} exit={true}>
-    <div className="project-description">
-      <div className="project-description__menu-container">
-        <ul className="wrapper project-description__menu">
-          <li>
-            <a className="project-description__name" href={link} target="blank">
-              <h2 className="heading-secondary">
-                {name}
-                <FaExternalLinkAlt className="project-description__name-icon" />
-              </h2>
-            </a>
-          </li>
-          <li>
-            <button>
-              <FaTimes
-                className="project-description__close-button"
-                onClick={() => closeProject()}
-              />
-            </button>
-          </li>
-        </ul>
-      </div>
-      <div className="project-description__content">
-        <div className="wrapper">
-          <div className="project-description__section">
-            <div className="project-description__links">
-              <Fade bottom>
-                <a
-                  className="project-description__link"
-                  href={repo}
-                  target="blank"
-                >
-                  <Button
-                    color="orange"
-                    shape="square"
-                    text="GitHub repository"
-                  />
-                </a>
-              </Fade>
-              <Fade bottom delay={50}>
-                <a
-                  className="project-description__link"
-                  href={link}
-                  target="blank"
-                >
-                  <Button color="orange" shape="square" text="Live Website" />
-                </a>
-              </Fade>
-            </div>
-          </div>
-          <div className="project-description__section">
-            <div className="project-description__section__text-container">
-              <Fade left delay={50}>
-                <span className="project-description__number">1</span>
-                <h3 className="heading-tertiary">General information</h3>
-                <p className="paragraph">{description}</p>
-              </Fade>
-            </div>
-            <div className="project-description__section__image-container">
-              <Fade right delay={50}>
-                <img
-                  className="project-description__section__image"
-                  src={images[0].url}
-                  alt={`${name} 1`}
+}) => {
+  const linksContainer = (
+    <div className="project-description__links">
+      <Fade bottom>
+        <a className="project-description__link" href={repo} target="blank">
+          <Button color="orange" shape="square" text="GitHub repository" />
+        </a>
+      </Fade>
+      <Fade bottom delay={50}>
+        <a className="project-description__link" href={link} target="blank">
+          <Button color="orange" shape="square" text="Live Website" />
+        </a>
+      </Fade>
+    </div>
+  );
+  return (
+    <Slide bottom duration={500} enter={true} exit={true}>
+      <div className="project-description">
+        <div className="project-description__menu-container">
+          <ul className="wrapper project-description__menu">
+            <li>
+              <a
+                className="project-description__name"
+                href={link}
+                target="blank"
+              >
+                <h2 className="heading-secondary">
+                  {name}
+                  <FaExternalLinkAlt className="project-description__name-icon" />
+                </h2>
+              </a>
+            </li>
+            <li>
+              <button>
+                <FaTimes
+                  className="project-description__close-button"
+                  onClick={() => closeProject()}
                 />
-              </Fade>
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div className="project-description__content">
+          <div className="wrapper">
+            <div className="project-description__section">{linksContainer}</div>
+            <div className="project-description__section">
+              <div className="project-description__section__text-container">
+                <Fade left delay={50}>
+                  <span className="project-description__number">1</span>
+                  <h3 className="heading-tertiary">General information</h3>
+                  <p className="paragraph">{description}</p>
+                </Fade>
+              </div>
+              <div className="project-description__section__image-container">
+                <Fade right delay={50}>
+                  <Image projectName={name} {...images[0]} />
+                </Fade>
+              </div>
             </div>
-          </div>
-          <div className="project-description__section">
-            <div className="project-description__section__image-container">
-              <Fade left delay={250}>
-                <img
-                  className="project-description__section__image"
-                  src={images[1].url}
-                  alt={`${name} 1`}
-                />
-              </Fade>
+            <div className="project-description__section">
+              <div className="project-description__section__image-container">
+                <Fade left delay={250}>
+                  <Image projectName={name} {...images[1]} />
+                </Fade>
+              </div>
+              <div className="project-description__section__text-container">
+                <Fade right delay={250}>
+                  <>
+                    <span className="project-description__number">2</span>
+                    <h3 className="heading-tertiary">Technologies used</h3>
+                    {technologies.map((list, idx) => (
+                      <div
+                        className="project-description__list-container"
+                        key={`${idx}_${list.name}`}
+                      >
+                        {list.name && (
+                          <h4 className="heading-quarternary">{list.name}:</h4>
+                        )}
+                        <List items={list.technologies} rows={1} />
+                      </div>
+                    ))}
+                  </>
+                </Fade>
+              </div>
             </div>
-            <div className="project-description__section__text-container">
-              <Fade right delay={250}>
-                <span className="project-description__number">2</span>
-                <h3 className="heading-tertiary">Technologies used</h3>
-                <List items={technologies} rows={1} />
-              </Fade>
+            <div className="project-description__section">
+              <div className="project-description__section__text-container">
+                <Fade left delay={250}>
+                  <>
+                    <span className="project-description__number">3</span>
+                    <h3 className="heading-tertiary">The process</h3>
+                    {process.map((item, idx) => (
+                      <p className="paragraph" key={idx}>
+                        {item}
+                      </p>
+                    ))}
+                  </>
+                </Fade>
+              </div>
+              <div className="project-description__section__image-container">
+                <Fade right delay={250}>
+                  <Image projectName={name} {...images[2]} />
+                </Fade>
+              </div>
             </div>
+            <div className="project-description__section">{linksContainer}</div>
           </div>
         </div>
       </div>
-    </div>
-  </Slide>
-);
+    </Slide>
+  );
+};
 
 export default ProjectDescription;
--- src/data.js
+++ src/data.js
@@ -1,15 +1,19 @@
 import project_1_1 from "./assets/img/projects/1_1.jpg";
 import project_1_2 from "./assets/img/projects/1_2.jpg";
+import project_1_3 from "./assets/img/projects/1_3.jpg";
 import project_2_1 from "./assets/img/projects/2_1.jpg";
 import project_2_2 from "./assets/img/projects/2_2.jpg";
-import project_3_1 from "./assets/img/projects/3_1.jpg";
+import project_2_3 from "./assets/img/projects/2_3.jpg";
+import project_4_1 from "./assets/img/projects/4_1.jpg";
+import project_4_2 from "./assets/img/projects/4_2.jpg";
+import project_4_3 from "./assets/img/projects/4_3.jpg";
 
 export const skills = [
   "HTML5",
   "CSS3",
   "Javascript",
   "React.js",
-  "SASS/SCSS",
+  "SASS",
   "Bootstrap 4",
   "Git",
   "Node.js",
@@ -23,34 +27,87 @@ export const projects = [
     name: "Mathew's Realty",
     description:
       "This is a website I created for Susan Mathews, a real estate professional and luxury home marketing specialist based in Westlake area. I was responsible for implementing all the markup, styling code and scripts. This website consists of 4 fully responsive and cross browser compatible pages.",
-    technologies: ["HTML", "CSS", "Javascript", "animate.css library"],
+    technologies: [
+      {
+        name: "Tech stack",
+        technologies: ["HTML", "CSS", "Javascript"]
+      },
+      {
+        name: "External libraries",
+        technologies: ["animate.css"]
+      }
+    ],
+    process: [
+      "Having discussed basic information about the website such as example websites, target audience, general structure and preferable color scheme, I created a website with thumbnail images and added content after receiving it from the client.",
+      "After publishing my work to the clients' domain, I did some minor changes in it and made the website responsive after the client was satisfied."
+    ],
     techStack: [0, 1, 2],
     repo: "https://github.com/dyaroshevych/mathews-realty",
     link: "https://dyaroshevych.github.io/mathews-realty",
     images: [
-      { url: project_1_1, description: "" },
-      { url: project_1_2, description: "" }
+      { url: project_1_1, description: "Properties page" },
+      { url: project_1_2, description: "Contact page" },
+      { url: project_1_3, description: "Home page" }
+    ]
+  },
+  {
+    name: "Advanced Trailer",
+    description:
+      "This is a single page application I created for Advanced Hemp Trailer, the largest supplier of processing machinery for the peanut, almond, and hazelnut industries. I was responsible for implementing all the markup, styling code and scripts. This website consists of a header and 5 responsive and cross browser compatible sections.",
+    technologies: [
+      {
+        name: "Tech stack",
+        technologies: ["React.js", "SCSS", "Javascript"]
+      },
+      {
+        name: "External libraries",
+        technologies: [
+          "react-reveal",
+          "react-scroll",
+          "react-player",
+          "react-icons"
+        ]
+      }
+    ],
+    process: [
+      "Having discussed basic information about the website such as example websites, target audience, general structure and preferable color scheme, I created a website with thumbnail images and added content after receiving it from the client.",
+      "After publishing my work to the clients' domain, I did some minor changes in it and made the website responsive after the client was satisfied."
+    ],
+    techStack: [3, 4, 2],
+    repo: "https://github.com/dyaroshevych/advanced-hemp-trailer",
+    link: "https://dyaroshevych.github.io/advanced-hemp-trailer",
+    images: [
+      { url: project_4_1, description: "Product section" },
+      { url: project_4_2, description: "Header" },
+      { url: project_4_3, description: "Image slider" }
     ]
   },
   {
     name: "Orbital View",
     description:
       "This is a website I created for Orbital View, an artificial intelligence company focused on roof damage assessment. I was responsible for implementing all the markup and styling code. This website consists of 4 fully responsive and cross browser compatible pages.",
-    technologies: ["HTML", "CSS", "Javascript", "animate.css library"],
+    technologies: [
+      {
+        name: "Tech stack",
+        technologies: ["HTML", "CSS", "Javascript"]
+      },
+      {
+        name: "External libraries",
+        technologies: ["animate.css"]
+      }
+    ],
+    process: [
+      "Having discussed basic information about the website such as example websites, target audience, general structure and preferable color scheme, I created a website with thumbnail images and added content after receiving it from the client.",
+      "After publishing my work to the clients' domain, I did some minor changes in it and made the website responsive after the client was satisfied."
+    ],
     techStack: [0, 1, 2],
     repo: "https://github.com/dyaroshevych/orbital-view",
     link: "https://orbitalview.ai",
     images: [
-      { url: project_2_1, description: "" },
-      { url: project_2_2, description: "" }
+      { url: project_2_1, description: "Home page" },
+      { url: project_2_2, description: "FAQ page" },
+      { url: project_2_3, description: "How it works page" }
     ]
-  },
-
-  {
-    name: "A-A-Agency",
-    techStack: [0, 1, 2],
-    description: "",
-    images: [{ url: project_3_1, description: "" }]
   }
 ];
 export const contact = [
@@ -73,3 +130,11 @@ export const contact = [
     placeholder: "Comments (optional)"
   }
 ];
+
+// import project_3_1 from "./assets/img/projects/3_1.jpg";
+// {
+//   name: "A-A-Agency",
+//   techStack: [0, 1, 2],
+//   description: "",
+//   images: [{ url: project_3_1, description: "" }]
+// }
